b216b0c5f48d2f630a0cadf49576c877b2f06c17
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-07-20 21:40:16 +0530

    Update aliv.py

--- userbot/plugins/aliv.py
+++ userbot/plugins/aliv.py
@@ -12,22 +12,7 @@ pm_caption = "`💠FRIDAY IS💠:` **ONLINE**\n\n"
 pm_caption += "**📥Moives Group📥** : `@cinema_lokamm`\n\n\n"
 pm_caption += "**📥My Group📥** : [🍿Join Movies Group🍿](https://t.me/joinchat/Oq1jlViv1uS2AkOG9MKChw)\n\n"
 
-pm_caption += "**🔖SYSTEM STATUS🔖**\n\n"
-pm_caption += "`⚖️TELETHON VERSION⚖️:` **6.0.9**\n`Python:` **3.7.4**\n\n"
-pm_caption += "`🎢DATABASE STATUS🎢:` **Functional**\n\n"
-pm_caption += "**🧮Current Branch🧮** : `master`\n\n"
-pm_caption += "**📬Friday OS📬** : `3.14`\n\n"
-pm_caption += "**💡Current Sat💡** : `StarkGangSat-2.25`\n\n"
-pm_caption += f"*💜*My Boss💜** : {DEFAULTUSER} \n\n"
-pm_caption += "**💥Heroku Database💥** : `AWS - Working Properly`\n\n"
-pm_caption += "**🚫License🚫** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
-pm_caption += "⛔️Copyright : By⛔️ [StarkGang@Github](GitHub.com/StarkGang)\n\n\n\n"
-pm_caption += " [┏┓━┏┓━━━━┏┓━┏┓━━━━━
-                 ┃┃━┃┃━━━━┃┃━┃┃━━━━━
-                 ┃┗━┛┃┏━━┓┃┃━┃┃━┏━━┓
-                 ┃┏━┓┃┃┏┓┃┃┃━┃┃━┃┏┓┃ 
-                 ┃┃━┃┃┃┃━┫┃┗┓┃┗┓┃┗┛┃ 
-                 ┗┛━┗┛┗━━┛┗━┛┗━┛┗━━┛](https://telegra.ph/FRIDAY-06-15)"
+pm_caption += " [┏┓━┏┓━━━━┏┓━┏┓━━━━━\n ┃┃━┃┃━━━━┃┃━┃┃━━━━━\n ┃┗━┛┃┏━━┓┃┃━┃┃━┏━━┓\n ┃┏━┓┃┃┏┓┃┃┃━┃┃━┃┏┓┃ \n ┃┃━┃┃┃┃━┫┃┗┓┃┗┓┃┗┛┃ \n ┗┛━┗┛┗━━┛┗━┛┗━┛┗━━┛](https://telegra.ph/FRIDAY-06-15)"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):

83ab8ce86300d0c8eb1585871c20165a7da83867
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-07-20 20:18:46 +0530

    Update aliv.py

--- userbot/plugins/aliv.py
+++ userbot/plugins/aliv.py
@@ -13,10 +13,6 @@ pm_caption += "**📥My Group📥** : [🍿Join Movies Group🍿](https://t.me/j
 
 pm_caption += f"*💜*My Boss💜** : {DEFAULTUSER} \n\n"
 pm_caption += "**💥Heroku Database💥** : `AWS - Working Properly`\n\n"
-pm_caption += "**🚫License🚫** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
-pm_caption += "⛔️Copyright : By⛔️ [StarkGang@Github](GitHub.com/StarkGang)\n\n\n\n"
-pm_caption += " [Deploy FridayUserbot](https://telegra.ph/FRIDAY-06-15)"
-#@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     chat = await alive.get_chat()
